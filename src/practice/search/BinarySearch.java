0206f1a35b07290fb6bd2bf6973d93190e6a301b
MichaelCS <mikalehc@gmail.com>
2019-09-09 06:17:14 +0800

    添加文件

---
+++ src/practice/search/BinarySearch.java
@@ -0,0 +1,21 @@
+package practice.search;
+
+/**
+ * @Classname BinarySearch
+ * @Description 简单的数组二分查找
+ * @Date 9/7/2019 9:20 AM
+ * @Created by MichaelCS
+ */
+public class BinarySearch {
+    /**
+     * 用于保存待查询数组
+     */
+    private Object[] array;
+
+    /**
+     * 用于保存待查询对象
+     */
+    private Object key;
+
+
+}
