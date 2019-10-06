574bb3957e6b96657e6f581100ad12af9d2af621
MichaelCS <mikalehc@gmail.com>
2019-10-06 18:33:35 +0800

    完成leetCode_214

--- src/leetCode/leet_214_ShortestPalindrome.java
+++ src/leetCode/leet_214_ShortestPalindrome.java
@@ -7,8 +7,8 @@ package leetCode;
  * @Created by MichaelCS
  */
 public class leet_214_ShortestPalindrome {
-    public static void main(String[] args){
-        String testString = "abcd";
+    public static void main(String[] args) {
+        String testString = "aaaaa";
         Solution solution = new Solution();
         System.out.println(solution.shortestPalindrome(testString));
 
@@ -17,7 +17,7 @@ public class leet_214_ShortestPalindrome {
 
 /**
  * 利用三个指针，head,middle,tail
- * 将问题转换为寻找最中心的对称点
+ * 将问题转换为寻找最中心的对称点（这一点是核心，所有逻辑以这一核心展开即可）
  * 未找到的话，三个指针向左移动继续寻找
  * 直到 head 到达 0 的位置，此时有两种情况
  * 若 head == tail 则以 middle 为中心
@@ -29,94 +29,144 @@ class Solution {
      * 处理不能用 3 个指针处理的情况
      * 即长度小于 3 的字符串
      */
-    private String handleExcep(String s){
-        if((s.length() == 0)||(s.length() == 1)){
+    private String handleExcep(String s) {
+        if ((s.length() == 0) || (s.length() == 1)) {
             return s;
-        }else if(s.length() == 2){
-            if(s.charAt(0) == s.charAt(1)){
+        } else if (s.length() == 2) {
+            if (s.charAt(0) == s.charAt(1)) {
                 return s;
-            }else {
-                return s.charAt(1)+s.charAt(0)+s;
+            } else {
+                return s.charAt(1) + s;
             }
-        }else {
+        } else {
             return "error!";
         }
     }
 
     public String shortestPalindrome(String s) {
-        // 初始化三个指针的位置
-        int tail = (int)Math.ceil((double) s.length()/2);
-        int middle = tail-1;
-        int head = tail-2;
+
+        int tail;
+        int middle;
+        int head;
+        boolean even;
+
+        // 初始化三个指针的位置及串长的奇偶性
+        if ((s.length() % 2) == 0) {
+            // 偶数情况
+            even = true;
+            tail = (int) Math.ceil((double) s.length() / 2) + 1;
+        } else {
+            // 奇数情况
+            even = false;
+            tail = (int) Math.ceil((double) s.length() / 2);
+        }
+        middle = tail - 1;
+        head = tail - 2;
+
+        // 起始拷贝点,这里用数组类型强行让他能在方法里面被改变
+        int[] copyPoint = {-1};
+        // 是否发现最左回文串标识
         boolean find = false;
+        // 结果
         String result;
 
         // 处理无法用三个指针处理的特殊情况
-        if(s.length() < 4){
+        if (s.length() < 3) {
             return handleExcep(s);
         }
         // 对串进行遍历
-        while(true){
-
-            // 对middle == tail 的情况进行处理
-            if(s.charAt(middle) == s.charAt(tail)){
-                int middle1 = middle;
-                int tail1 = tail;
-
-                while (true){
-                    // 一直到 middle 移动到字符串头部跳出循环
-                    if(middle1 == 0){
-                        find = true;
-                        break;
-                    }
-                    // 以原 middle tail 为中心往两边拓展
-                    if(s.charAt(middle1-1) == s.charAt(tail1+1)){
-                        middle1--;
-                        tail1++;
-                    }else {
-                        break;
-                    }
-                }
-
-                if(find) {
-                    // 构造结果串
-                    // 将tail后面的子串逆序加到头部
-                    StringBuilder builder = new StringBuilder(s.substring(tail1+1));
-                    result = builder.reverse()+s;
-                    break;
-                }
+        while (head >= 0) {
+            if(even){
+                find = checkHeadAndMiddle(head,middle,tail,s,copyPoint);
+                if(find)break;
+                find = checkHeadAndTail(head,middle,tail,s,copyPoint);
+                if(find)break;
+            }else {
+                find = checkHeadAndTail(head,middle,tail,s,copyPoint);
+                if(find)break;
+                find = checkHeadAndMiddle(head,middle,tail,s,copyPoint);
+                if(find)break;
             }
-
-            // 对head == tail 的情况进行处理
-            if(s.charAt(head) == s.charAt(tail)){
-                int head1 = head;
-                int tail1 = tail;
-                while(true){
-                    if(head1 == 0){
-                        find = true;
-                        break;
-                    }
-                    if(s.charAt(head1-1) == s.charAt(tail+1)){
-                        head1--;
-                        tail1++;
-                        // head1 遍历到串头说明已找到
-                    }else {
-                        break;
-                    }
-                }
-
-                if(find) {
-                    // 构造结果串
-                    // 将tail后面的子串逆序加到头部
-                    StringBuilder builder = new StringBuilder(s.substring(tail1+1));
-                    result = builder.reverse()+s;
-                    break;
-                }
-            }
-            tail--;
-            middle--;
             head--;
+            middle--;
+            tail--;
         }
+
+
+    StringBuilder builder;
+        if (find) {
+        // 构造结果串
+        // 将tail后面的子串逆序加到头部
+        builder = new StringBuilder(s.substring(copyPoint[0] + 1));
+        result = builder.reverse() + s;
+    } else {
+        // 未找到
+        builder = new StringBuilder(s.substring(1));
+        result = builder.reverse() + s;
+    }
         return result;
+    }
+
+    private boolean checkHeadAndMiddle(int head, int middle, int tail, String s, int[] copyPoint) {
+        boolean find = false;
+        // 对head == 0 且 middle == head  的特殊情况进行处理
+        if (s.charAt(head) == s.charAt(middle)) {
+            if (head == 0 && s.charAt(head) != s.charAt(tail)) {
+                find = true;
+                copyPoint[0] = middle;
+                return find;
+            }
+
+            int head1 = head;
+            int middle1 = middle;
+
+            while ((head1 - 1 >= 0) && (middle1 + 1 < s.length())) {
+                // 以原 middle tail 为中心往两边拓展
+                if (s.charAt(head1 - 1) == s.charAt(middle1 + 1)) {
+                    head1--;
+                    middle1++;
+                    // 一直到 middle 移动到字符串头部跳出循环
+                    if (head1 == 0) {
+                        copyPoint[0] = (Integer) middle1;
+                        find = true;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return find;
+    }
+
+    private boolean checkHeadAndTail(int head, int middle, int tail, String s, int[] copyPoint) {
+        boolean find = false;
+
+        if (s.charAt(head) == s.charAt(tail)) {
+            if(head == 0){
+                find = true;
+                copyPoint[0] = tail;
+                return find;
+            }
+            int head1 = head;
+            int tail1 = tail;
+
+            while ((head1 - 1 >= 0) && (tail1 + 1 < s.length())) {
+                // 以原 middle 为中心往两边拓展
+                if (s.charAt(head1 - 1) == s.charAt(tail1 + 1)) {
+                    head1--;
+                    tail1++;
+                    // 一直到 head 移动到字符串头部跳出循环
+                    if (head1 == 0) {
+                        copyPoint[0] = tail1;
+                        find = true;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return find;
     }
 }
---
+++ src/practice/test/IntegerTest.java
@@ -0,0 +1,67 @@
+package practice.test;
+
+/**
+ * @Classname IntegerTest
+ * @Description Integer 如果表示范围为 -128 —— 127 不会创建对象，表示范围超出这个范围时才会创建对象！！！
+ * @Date 10/6/2019 11:06 AM
+ * @Created by MichaelCS
+ */
+public class IntegerTest {
+
+    private static class Test{
+        int a;
+        void setA(int a){
+            this.a = a;
+        }
+    }
+
+    private boolean IntegerFunc(Test test){
+//        integer++;
+//        System.out.println(integer.equals(integer));
+//        System.out.println(System.identityHashCode(integer));
+        test.setA(2);
+        return true;
+    }
+
+    private boolean StringFunc(String s){
+        s = new String("changed");
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        Integer integer = 12223;
+        System.out.println(System.identityHashCode(integer));
+        String s = new String("origin");
+        Test test = new Test();
+        test.a = 1;
+
+        IntegerTest referenceTest = new IntegerTest();
+
+        // 测试新建类引用
+        referenceTest.IntegerFunc(test);
+        System.out.println(test.a);
+
+//        // 测试Integer 是否会被修改
+//        referenceTest.IntegerFunc(integer);
+//        System.out.println(integer);
+
+//        // 测试String是否会被修改
+//        referenceTest.StringFunc(s);
+//        System.out.println(s);
+
+        Integer a = 12;
+        Integer b = 12;
+
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+
+        Integer c = 128;
+        Integer d = 128;
+
+        System.out.println(c==d);
+        System.out.println(c.equals(d));
+    }
+}
+
+
